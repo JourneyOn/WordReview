384d019572816364f193415f8160ffaafc39b608
John <shu.peng@coscico.com>
2014-05-04 11:58:26 +0800

    init

---
+++ WordReview/WordReview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WordReview
+//
+//  Created by shupeng on 5/4/14.
+//  Copyright (c) 2014 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
